951f09fbadbe098b6ae426c4d2048af4912baed5
Jimmy Charnley Kromann, dfi-viking <jimmy@charnley.dk>
2012-07-04 23:06:18 +0200

    update and cleanup

--- interface/index.php
+++ interface/index.php
@@ -3,13 +3,15 @@
 <head>
 	<meta charset="utf-8">
 	
-	<title>GAMESS and OpenBabel Interface - GOBI</title>
+	<title>GAMESS and OpenBabel Interface V2 - GOBI2</title>
 	
 	<link rel="stylesheet" href="style/screen.css" >
 	
 	<script type="text/javascript" src="script/jmol/Jmol.js"></script>
 	<script type="text/javascript" src="script/jquery-1.7.2.min.js"></script>
 	<script type="text/javascript" src="script/gobi_main.js"></script>
+	
+
 	
 </head>
 <body>
@@ -38,7 +40,28 @@
 		<section class="threecolumn">
 			
 			<section class="column alpha">
-
+				
+				<br />
+				<br />
+				
+				<strong>Molecule Actions</strong>
+				
+				<div class="actions">
+					<ul>
+						<li><a href="#">Reset</a></li>
+						<li>
+							<select>
+								<option>Select</option>
+								<option>Caffine</option>
+								<option>Water</option>
+							</select>
+							<br />
+							<a href="#">Load</a>
+						</li>
+						<li><a href="#">Delete Atom</a></li>
+					</ul>
+				</div>
+				
 			</section>
 			
 			<section class="column beta">
@@ -46,25 +69,28 @@
 			</section>
 			
 			<section class="column gamma">
+
+				<!-- Initialize Jmol -->
+				<script>
+				  jmolInitialize("script/jmol");
+				  jmolCheckBrowser("popup", "browsercheck", "onClick");
+				</script>
 				
-<div class="add_atom_to_molecule">
-
-<?php include('includes/periodic.inc.php');?>
-
-</div>
-
-
-<script>
-  jmolInitialize("script/jmol");
-  jmolCheckBrowser("popup", "browsercheck", "onClick");
-</script>
-
-<div class="canvas">
-<script>
-jmolApplet(460, "load caffeine.xyz.gz");
-</script>
-</div>
+				<div class="canvas">
+					<script>
+						//jmolApplet(460);
+						jmolApplet(460, "load caffeine.xyz.gz");
+					</script>
+				</div>
+					
+				<div class="add_atom_to_molecule">
+					<?php include('includes/periodic.inc.php');?>
+				</div>
 				
+				<br />
+				<br />
+				<br />
+				<br />
 				
 			</section>
 			<div class="clean"></div>
--- interface/script/gobi_main.js
+++ interface/script/gobi_main.js
@@ -11,12 +11,18 @@ $(function() {
 		var atom = 
 		{
 			'sym': $(this).html(),
-			'num': $(this).parent().find('.number').html(),
-			'wei': $(this).parent().find('.weight').html()
+			'num': parseInt($(this).parent().find('.number').html()),
+			'wei': parseFloat($(this).parent().find('.weight').html())
 		}
 		
 		$(this).click(function() 
 		{
+			
+			if(atom['num'] > 16 ){
+					alert('too high');
+					return false;
+			}
+			
 			
 			jmolScript('set picking assignAtom_'+atom['sym']);
 			$('.periodictable .symbol.active').removeClass('active');
